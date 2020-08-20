d896e8f0a00c34db069edbf478cc2326c989f71d
Noman Shoukat <nomanaadma@gmail.com>
2020-08-20 16:57:06 +0500

    UseEffect Hook

--- src/hooks/CounterF.jsx
+++ src/hooks/CounterF.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Counterf(props) {
     
     const [ count, setCount] = useState(0);
     const [ name, setName] = useState('');
+
+    // componentDidMount
+    // componentDidUpdate
+    // componentWillUnmount
+    useEffect(() => {
+        document.title = `${name} has clicked ${count} times`;
+       
+        return () => {
+            console.log('Clean Up');
+        };
+    });
     
     return (
         <React.Fragment>
             <input type="text" onChange={e => setName(e.currentTarget.value) } />
             <div>{name} has clicked {count} times</div>
-            <button onClick={ () => setState(count + 1) }>Increase</button>
+            <button onClick={ () => setCount(count + 1) }>Increase</button>
         </React.Fragment>
     );
 }
