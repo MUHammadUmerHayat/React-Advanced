f88f897a807fc0d87f595ac6d1ed877f3e7bcb56
Noman Shoukat <nomanaadma@gmail.com>
2020-08-20 16:47:50 +0500

    Another state example in functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import CounterC from './hooks/CounterC';
 import CounterF from './hooks/CounterF';
 
 function App() {
   return (
     <React.Fragment>
-        <CounterC />
         <CounterF />
     </React.Fragment>
   );
--- src/hooks/CounterF.jsx
+++ src/hooks/CounterF.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 
 function Counterf(props) {
     
-    const [ count, setState] = useState(0);
+    const [ count, setCount] = useState(0);
+    const [ name, setName] = useState('');
     
     return (
         <React.Fragment>
-                <div>Count: {count}</div>
-                <button onClick={ () => setState(count + 1) }>Increase</button>
+            <input type="text" onChange={e => setName(e.currentTarget.value) } />
+            <div>{name} has clicked {count} times</div>
+            <button onClick={ () => setState(count + 1) }>Increase</button>
         </React.Fragment>
     );
 }
